9330f37d0fe8816c21fdda7150c0ecf280c57fc6
kevin eboda <kevineboda@gmail.com>
2024-04-04 14:50:33 +0100

    likes appear automatically

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react"; // added
 //import { useState, useEffect } from "react"; commented this out
 import { useNavigate } from "react-router-dom";
 import { getPosts } from "../../services/posts";
+//import { likePost } from "../../services/like"; //added import
 import Post from "../../components/Post/Post";
 import CreatePost from "../../components/Post/CreatePost";
 import Navbar from "../../components/NavBar/Navbar";
 import UserDetails from "../../components/User/UserDetails";
+import LikeButton from "../../components/LikeButton/LikeButton"; //added import
 import "./FeedPage.css";
 
 export const FeedPage = () => {
@@ -30,7 +32,7 @@ export const FeedPage = () => {
 
    useEffect(() => { //added
      fetchPosts(); // Fetch posts when component mounts 
-      //fetch posts every 10 seconds 
+      //fetch posts every 5 seconds 
      const interval = setInterval(fetchPosts, 5000); 
      return () => clearInterval(interval); // Cleanup interval on component unmount 
    }, [navigate]); // dont know how this works
@@ -40,32 +42,48 @@ export const FeedPage = () => {
      return null; // Return null if token is not available
    }
 
+   // new function to update posts with new like status
+   const updatePostLike = (postId, isLiked) => {
+    const updatedPosts = posts.map((post) => {
+      if (post._id === postId) {
+        return { ...post, isLiked };
+      }
+      return post;
+    });
+    setPosts(updatedPosts);
+  };
+
+   // new function ends here 
+
+
+   
 
    return (
-     <>
-       <Navbar />
-       <UserDetails />
-       <div className="feed-page">
-       {/* <h1>Hello {fullName}</h1> */}
-         <h2>Your feed</h2>
-         <div className="createpost" role="feed">
-           <CreatePost />
-         </div>
-         <div className="post_list" role="feed">
-           {posts.map((post) => (
-             <Post post={post} key={post._id} />
-           ))}
-         </div>
-       </div>
-     </>
-   );
- };
-
-
-
-
-
-
+    <>
+      <Navbar />
+      <UserDetails />
+      <div className="feed-page">
+        <h2>Your feed</h2>
+        <div className="createpost" role="feed">
+          <CreatePost />
+        </div>
+        <div className="post_list" role="feed">
+          {posts.map((post) => (
+            <div key={post._id}>
+              <Post post={post} />
+              <LikeButton
+                postId={post._id}
+                userId={post.userId}
+                isLiked={post.isLiked}
+                updatePost={updatePostLike}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 
 
